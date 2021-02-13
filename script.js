6d3c24f952711016f46855becd0539be971d93e5
Niatse <75042389+Niatse@users.noreply.github.com>
2021-02-12 23:26:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 var ryzen3 = {
 	 "Nombre": "Ryzen 3 3200g",
      "Precio":"140USD",
-     "Nucleos":"4",
+     "Nucleos":"4 nucleos",
      "RelojBase":"3.6Ghz",
      "id": 3
 }; 
@@ -10,7 +9,7 @@ var ryzen3 = {
 var ryzen5 = {
 	"Nombre":"Ryzen 5 3400g",
 	"Precio":"200USD",
-	"Nucleos":"4",
+	"Nucleos":"4 nucleos",
 	"RelojBase":"3.7Ghz",
      "id": 5
 };
@@ -18,7 +17,7 @@ var ryzen5 = {
 var ryzen7 = {
   "Nombre":"Ryzen 7 3700x",
 	"Precio":"550USD",
-	"Nucleos":"8",
+	"Nucleos":"8 nucleos",
 	"RelojBase":"3.6Ghz",
     "id": 7
 };
@@ -28,23 +27,12 @@ var codigo = Number(prompt("Digite el ID del producto"))
 
 var existeProducto = procesadores.findIndex(procesador => procesador.id == codigo)
 
-if(existeProducto == 0){
-   alert("El procesador"+" "+ryzen3.Nombre+" "+"Tiene un precio de"+" "+ryzen3.Precio+" "+"Cuenta con"+" "+ryzen3.Nucleos+" "+"Nucleos"+" "+"Y un reloj base de:"+" "+ryzen3.RelojBase)
+if(existeProducto >= 0)
+{
+alert("El procesador"+" "+procesadores[existeProducto].Nombre+" "+"Tiene un precio de "+procesadores[existeProducto].Precio+" "+"Cuenta con " +procesadores[existeProducto].Nucleos+" y un reloj base de "+procesadores[existeProducto].RelojBase)
 
 }
-
-if (existeProducto == 1) {
-	 alert("El procesador"+" "+ryzen5.Nombre+" "+"Tiene un precio de"+" "+ryzen5.Precio+" "+"Cuenta con"+" "+ryzen5.Nucleos+" "+"Nucleos"+" "+"Y un reloj base de:"+" "+ryzen5.RelojBase)
+else
+{
+alert("El producto con la id"+" "+codigo+" "+ "no existe,lo sentimos.")
 }
-
-if (existeProducto == 2) {
-	 alert("El procesador"+" "+ryzen7.Nombre+" "+"Tiene un precio de"+" "+ryzen7.Precio+" "+"Cuenta con"+" "+ryzen7.Nucleos+" "+"Nucleos"+" "+"Y un reloj base de:"+" "+ryzen7.RelojBase)
-}
-if (existeProducto == -1) {
-	alert("El producto con la id"+" "+codigo+" "+ "no existe,lo sentimos.")
-}
-
-
-
-
-
